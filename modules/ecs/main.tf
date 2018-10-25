20fd70ad280db4973ef71128d65e85b87c217949
Matt Rosenquist <mrosenquist@and.digital>
2018-10-25 14:36:37 +0100

    Update

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -13,12 +13,13 @@ module "ecs_instances" {
   source = "../ecs_instances"
   enviroment = "${var.enviroment}"
   instance_group = "${}"
-  aws_aim = "${}"
-  private_subnet_ids = "${}"
-  instance_type = "${}"
+  aws_aim = "${var.aws_aim}"
+  private_subnet_ids = "${module.network.private_subnet_ids}"
+  instance_type = "${var.instance_type}"
   min_size = "${var.min_size}"
   max_size = "${var.max_size}"
   desired_capacity = "${var.desired_capacity}"
+  vpc_id = "${module.network.vpc_id}"
 }
 
 resource "aws_ecs_cluster" "cluster" {
--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,4 +1,36 @@
 
 variable "enviroment" {}
 
-variable "vpc_id" {}
+variable "cidr" {}
+
+variable "min_size" {
+  default = 2
+}
+
+variable "max_size" {
+  default = 2
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "private_cidrs" {
+  type = "list"
+}
+
+variable "public_cidrs" {
+  type = "list"
+}
+
+variable "availability_zones" {
+  type = "list"
+}
+
+variable "aws_aim" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
