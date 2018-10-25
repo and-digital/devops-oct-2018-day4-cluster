296793ef77a4388fda082cee62973820a8fe6c69
Matt Rosenquist <mrosenquist@and.digital>
2018-10-25 11:41:55 +0100

    Update backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,17 @@
 terraform {
   backend "s3" {
-    bucket = "day4-ex1-ecs-cluster"
-    key    = "path/to/my/key"
+    bucket = "${var.aws_state_bucket}}"
+    key    = "${var.aws_state_bucket_key}"
+    region = "${var.region}"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.aws_state_bucket}}"
+    key    = "${var.aws_state_bucket_key}"
     region = "${var.region}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,6 +3,15 @@ variable "region" {
   default = "eu-west-1"
 }
 
+variable "aws_state_bucket" {
+  default = "day4-ex1-ecs-cluster"
+}
+
+variable "aws_state_bucket_key" {
+  default = "ecs-cluster.tfstate"
+}
+
+
 variable "aws_key" {}
 
 variable "aws_secret" {}
